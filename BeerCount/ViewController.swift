b23d3edfddea9990dd145534a3790b773ac74911
touyou <fujiyou1121@gmail.com>
2019-10-13 17:44:15 +0900

    Initial Commit

---
+++ BeerCount/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeerCount
+//
+//  Created by 藤井陽介 on 2019/10/13.
+//  Copyright © 2019 touyou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
